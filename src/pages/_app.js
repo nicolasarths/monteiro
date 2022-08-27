f0663763eb4663fa1e478b3904cd9f26fee5a7e1
nicolasarths <nicolasarths@hotmail.com>
2022-08-27 20:06:45 -0300

    botão whatsapp e header adicionados

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
+import Layout from '../components/layout'
 import '../styles/globals.css'
 import '../styles/main.css'
+import '../styles/layout.css'
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <Layout>
+    <Head>
+      <meta name="viewport" content="width=device-width"/>
+    </Head>
+    <Component {...pageProps} />
+  </Layout>
+  )
 }
 
 export default MyApp
