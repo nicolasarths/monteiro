96246d4856a35edd5b2ec8bb1c182b94fa97dd1b
nicolasarths <nicolasarths@hotmail.com>
2022-08-29 18:17:16 -0300

    adicionadas imagens e textos discritivos

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,49 +15,75 @@ export default function Home() {
             Guincho de Moto
           </h2>
           <p>
-            Transporte de motos realizado com total
-            segurança através do nosso sistema único
-            com rampa automatizada, 24 horas por dia,
-            onde quer que você esteja!
+            Transporte realizado com agilidade e segurança
+            24 horas na capital Porto Alegre e região metropolitana.
+            Demais localidades com agendamento.
           </p>
         </div>
         <div className="grid-image grid-item-2">
           <Image
-            alt="imagem de exemplo 2"
+            alt="imagem de exemplo de guincho de moto"
             layout="fill"
-            src="/example-2.jpg" />
-        </div>
-
-        
-        <div className="grid-image grid-item-3">
-          <Image
-            alt="imagem de exemplo 1"
-            layout="fill"
-            src="/example-1.jpg" />
+            src="/exemplo-guincho-de-moto-3.jpg" />
         </div>
 
         <div className="grid-text grid-item-4">
           <h2 className="grid-title">
-            Atendimento 24h
+            Transporte de motos
           </h2>
-          <ul>
-            <li>
-              Leva e traz de moto para revisão em oficinas,
-              realizar vistorias no Detran, lavagem, entre outros;
-            </li>
-            <li>
-              Transporte de motos para o litoral, interior e outros estados;
-            </li>
-            <li>
-              Resgate de moto em caso de acidente, pane mecânica e falta de gasolina;
-            </li>
-          </ul>
+          <p>
+            Transporte de motos para revisões em oficinas,
+            realização de vistorias, para o litoral, interior
+            e outros estados.
+          </p>
         </div>
+
+        <div className="grid-image grid-item-3">
+          <Image
+            alt="imagem de exemplo de transporte de moto"
+            layout="fill"
+            src="/exemplo-transporte-1.jpg" />
+        </div>
+
+        <div className="grid-text grid-item-5">
+          <h2 className="grid-title">
+            Resgate e socorro
+          </h2>
+          <p>
+            Resgate e socorro de motos em caso de acidente,
+            pane mecânica, pane seca, entre outros.
+          </p>
+        </div>
+
+        <div className="grid-image">
+          <Image
+            alt="imagem de exemplo de resgate de motos"
+            layout="fill"
+            src="/exemplo-resgate.jpg" />
+        </div>
+
+        
+        <div className="grid-text grid-item-6">
+          <h2 className="grid-title">
+            Reboque de motos
+          </h2>
+          <p>
+            Reboque de motos com reboque basculante 24h.
+          </p>
+        </div>
+
+        <div className="grid-image">
+          <Image
+            alt="imagem de exemplo de reboque de motos"
+            layout="fill"
+            src="/exemplo-reboque.jpg" />
+        </div>
+
 
       </main>
       <span className="rescue-button rescue-button-bg hide">
         <a href="https://wa.me/5551986561694">
-          <FontAwesomeIcon icon={faWhatsapp} className="rescue-button-whatsapp-icon rescue-button-bg"/>
+          <FontAwesomeIcon icon={faWhatsapp} className="rescue-button-whatsapp-icon rescue-button-bg" />
           Solicitar resgate
         </a>
       </span>
