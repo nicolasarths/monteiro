d782d51042603e74965f62190e45fc2943fa1358
nicolasarths <nicolasarths@hotmail.com>
2022-09-07 16:19:26 -0300

    consertando formulário de contato agora envia para endereço correto e a mensagem enviada foi atualizada

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -5,33 +5,35 @@ mail.setApiKey(process.env.SENDGRID_API_KEY)
 
 export default async function handler(req, res) {
   const formObject = JSON.parse(req.body)
-  const lines = Object.entries(formObject).map(obj => {
-    return `
-      <strong>${obj[0]}</strong>: ${obj[1]}<br/>
-    `
-  })
-  let message = ""
-  lines.forEach(line => message+=line)
+  
+  let message = `
+    Mensagem de <strong>${formObject.nome}</strong>. <br/>
+    Telefone para contato: <strong>${formObject.telefone}</strong>.<br/>
+    <br/>
+    <strong>Mensagem:</strong><br/>
+    ${formObject.mensagem}
+  `
 
-  const subject = `Mensagem ID#${uniqueIdentifier()}`;
+  const subject = `Mensagem de ${formObject.nome} - ID #${uniqueIdentifier()}`;
   const text = JSON.stringify(formObject);
   const html = message
 
   const data = {
-    to: 'contato@monteiromototransporte.com.br',
-    from: 'nicolasarths.sender@gmail.com',
+    to: 'volneimonteirors@hotmail.com',
+    from: 'admin@nicolasarths.com.br',
     subject,
     text,
     html
   }
 
   try {
-    const response = await mail.send(data)
-    if (response.statusCode > 300) res.status(500).json({status: 'Fail'})
-    else res.status(200).json({ status: 'Ok' })
+    const [response, string] = await mail.send(data)
+    console.log(response)
+    if (response.statusCode > 199 && response.statusCode < 300) res.status(200).json({status: 'Ok'})
+    else res.status(500).json({ status: 'Fail' })
   } catch (error) {
     console.log(error)
-    res.status(500).json({status: 'Fail'})
+    res.status(500).json({ status: 'Fail' })
   }
   
   
