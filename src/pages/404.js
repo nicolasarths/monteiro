1f5dcd31f471b8d2acc2c56b0fd7abd207d8ecf9
nicolasarths <nicolasarths@hotmail.com>
2022-10-15 18:24:09 -0300

    organize page exports into common/pages folder, also data

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,2 @@
-import Head from "next/head"
-import Link from "next/link"
-
-export default function NotFound() {
-    return (<>
-        <Head>
-            <title>Monteiro Guincho de Moto - Página não encontrada</title>
-        </Head>
-        <div className="not-found padding-small">
-            <h1 className="not-found-title primary-red">Página não encontrada</h1>
-            <p>Opa, parece que essa página não está disponível.</p>
-            <p className="primary-red">
-                <Link href="/">Retornar para a página inicial</Link>
-            </p>
-        </div>
-    </>)
-}
+import NotFound from "common/pages/404"
+export default NotFound;
