f35674d708968ea77eb1c4fe8c9c21738157ebb4
nicolasarths <nicolasarths@hotmail.com>
2022-10-16 00:39:52 -0300

    abstract styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,6 @@ import Head from 'next/head'
 import Layout from 'common/layouts/main'
 import '../styles/globals.css'
 import '../styles/main.css'
-import '../styles/layout.css'
-import '../styles/agendar.css'
-import '../styles/not-found.css'
 
 
 function MyApp({ Component, pageProps }) {
