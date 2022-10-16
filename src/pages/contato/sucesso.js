dc288c2f54942a0c54b9f0aaedf6583753104f31
nicolasarths <nicolasarths@hotmail.com>
2022-10-16 00:34:07 -0300

    abstract contact page

--- src/pages/contato/sucesso.js
+++ src/pages/contato/sucesso.js
@@ -1,15 +1,2 @@
-import Head from "next/head";
-import Link from "next/link";
-
-export default function Sucesso(){
-    return (<>
-    <Head>
-        <title>Monteiro Guincho de Moto - Contato - Sucesso</title>
-      </Head>
-        <div className="form-success white padding-small">
-        <h1 className="primary-red form-success-title">Sucesso!</h1>
-        <p>Recebemos sua mensagem e em breve iremos lhe retornar!</p>
-        <p className="primary-red"><Link href="/">Retornar para a página inicial</Link></p>
-        </div>
-    </>)
-}
+import Success from "common/pages/Contact/Success"
+export default Success;
