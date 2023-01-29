10012adf1b914ff51ddee134e0308a83f6069576
nicolasarths <nicolasarths@hotmail.com>
2023-01-29 15:35:48 -0300

    Fix (Presentation Section): Fix properties on iframe.

--- src/common/pages/Homepage/PresentationSection/index.js
+++ src/common/pages/Homepage/PresentationSection/index.js
@@ -50,9 +50,9 @@ const PresentationSection = () => {
       <iframe
         src="https://www.youtube.com/embed/Zw9nMCUJNRY"
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
