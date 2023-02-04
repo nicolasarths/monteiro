820f41b0314d45da3f12d0339a93ef4d19bb45e5
nicolasarths <nicolasarths@hotmail.com>
2023-02-03 23:29:33 -0300

    Enhancement (Homepage): Enhance homepage visualization: change carousel images.

--- src/common/pages/Homepage/images/index.js
+++ src/common/pages/Homepage/images/index.js
@@ -1,34 +1,99 @@
-import FIRST_EXAMPLE from "./exemplo-guincho-de-moto-3.jpg";
-import SECOND_EXAMPLE from "./exemplo-transporte-1.jpg";
-import THIRD_EXAMPLE from "./exemplo-resgate.jpg";
-import FOURTH_EXAMPLE from "./exemplo-reboque.jpg";
+import FIRST_EXAMPLE from "./exemplo-1.png";
+import FIRST_EXAMPLE_SMALL from "./exemplo-1-small.png";
+import SECOND_EXAMPLE from "./exemplo-2.png";
+import SECOND_EXAMPLE_SMALL from "./exemplo-2-small.png";
+import THIRD_EXAMPLE from "./exemplo-3.png";
+import THIRD_EXAMPLE_SMALL from "./exemplo-3-small.png";
+import FOURTH_EXAMPLE from "./exemplo-4.png";
+import FOURTH_EXAMPLE_SMALL from "./exemplo-4-small.png";
+import FIFTH_EXAMPLE from "./exemplo-5.png";
+import FIFTH_EXAMPLE_SMALL from "./exemplo-5-small.png";
+import SIXTH_EXAMPLE from "./exemplo-6.png";
+import SIXTH_EXAMPLE_SMALL from "./exemplo-6-small.png";
+import SEVENTH_EXAMPLE from "./exemplo-7.png";
+import SEVENTH_EXAMPLE_SMALL from "./exemplo-7-small.png";
+import EIGHTH_EXAMPLE from "./exemplo-8.png";
+import EIGHTH_EXAMPLE_SMALL from "./exemplo-8-small.png";
+
+const EXAMPLES = [
+  {
+    big: FIRST_EXAMPLE,
+    small: FIRST_EXAMPLE_SMALL,
+  },
+  {
+    big: SECOND_EXAMPLE,
+    small: SECOND_EXAMPLE_SMALL,
+  },
+  {
+    big: THIRD_EXAMPLE,
+    small: THIRD_EXAMPLE_SMALL,
+  },
+  {
+    big: FOURTH_EXAMPLE,
+    small: FOURTH_EXAMPLE_SMALL,
+  },
+  {
+    big: FIFTH_EXAMPLE,
+    small: FIFTH_EXAMPLE_SMALL,
+  },
+  {
+    big: SIXTH_EXAMPLE,
+    small: SIXTH_EXAMPLE_SMALL,
+  },
+  {
+    big: SEVENTH_EXAMPLE,
+    small: SEVENTH_EXAMPLE_SMALL,
+  },
+  {
+    big: EIGHTH_EXAMPLE,
+    small: EIGHTH_EXAMPLE_SMALL,
+  },
+];
 
 const images = [
   {
     alt: "caminhonete cinza com moto honda estradeira azul em cima, guincho de motos em porto alegre",
-    src: FIRST_EXAMPLE,
-    type: "bottom",
+    src: EXAMPLES[0],
     caption:
-      "Transporte realizado com agilidade e segurança 24 horas na capital Porto Alegre e região metropolitana. Demais localidades com agendamento.",
+      "Transporte realizado com agilidade e segurança 24 horas na capital Porto Alegre e região metropolitana.",
   },
 
   {
-    alt: "caminhonete cinza com moto harley davidson azul bebê em cima, guincho de motos em porto alegre",
-    src: SECOND_EXAMPLE,
-    caption:
-      "Transporte de motos para revisões em oficinas, realização de vistorias, para o litoral, interior e outros estados.",
+    alt: "moto harley davidson laranja em cima de reboque basculante, guincho de motos em porto alegre",
+    src: EXAMPLES[1],
+    caption: "Reboque de motos com reboque basculante 24h.",
   },
   {
     alt: "caminhonete cinza com moto estradeira marrom ferro libre em cima, guincho de motos em porto alegre",
-    src: THIRD_EXAMPLE,
-    type: "top",
+    src: EXAMPLES[2],
     caption:
       "Resgate e socorro de motos em caso de acidente, pane mecânica, pane seca, entre outros.",
   },
   {
-    alt: "caminhonete cinza com moto harley davidson  laranja em cima, guincho de motos em porto alegre",
-    type: "top",
-    src: FOURTH_EXAMPLE,
+    alt: "caminhonete cinza com moto harley davidson azul bebê em cima, guincho de motos em porto alegre",
+    src: EXAMPLES[3],
+    caption:
+      "Agendamento de transporte de motocicletas para o litoral, outras cidades e até mesmo outros estados",
+  },
+  {
+    alt: "caminhonete cinza com moto harley davidson azul bebê em cima, guincho de motos em porto alegre",
+    src: EXAMPLES[4],
+    caption: "Reboque de motos com reboque basculante 24h.",
+  },
+  {
+    alt: "caminhonete cinza com moto harley davidson azul bebê em cima, guincho de motos em porto alegre",
+    src: EXAMPLES[5],
+    caption:
+      "Transporte de motos para revisões em oficinas, realização de vistorias, para o litoral, interior e outros estados.",
+  },
+  {
+    alt: "caminhonete cinza com moto harley davidson azul bebê em cima, guincho de motos em porto alegre",
+    src: EXAMPLES[6],
+    caption: "Reboque de motos com reboque basculante 24h.",
+  },
+  {
+    alt: "caminhonete cinza com moto harley davidson azul bebê em cima, guincho de motos em porto alegre",
+    src: EXAMPLES[7],
     caption: "Reboque de motos com reboque basculante 24h.",
   },
 ];
