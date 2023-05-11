ba814fcd667f90143b8c9279175ca0d04b0f8157
nicolasarths <nicolasarths@hotmail.com>
2023-05-10 23:35:00 -0300

    Fix (Redirection): currently working on a problem with redirection.

--- src/common/pages/404/index.js
+++ src/common/pages/404/index.js
@@ -1,19 +1,41 @@
-import Head from "next/head"
-import Link from "next/link"
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-import styles from "./NotFound.module.sass"
+import styles from "./NotFound.module.sass";
 
 const NotFound = () => {
-    return (<>
-        <Head>
-            <title>Monteiro Guincho de Moto - Página não encontrada</title>
-        </Head>
-        <div className={styles.notFound}>
-            <h1>Página não encontrada</h1>
-            <p>Opa, parece que essa página não está disponível.</p>
-            <Link href="/">Retornar para a página inicial</Link>
-        </div>
-    </>)
-}
+  const { push } = useRouter();
+
+  useEffect(() => {
+    push("/");
+  }, []);
+  return (
+    <>
+      <Head>
+        <title>Monteiro Guincho de Moto - Redirecionando</title>
+      </Head>
+      <div className={styles.notFound}>
+        <h1>Redirecionando</h1>
+        <p>
+          Se você não for redirecionado para a página inicial, clique no botão
+          abaixo.
+        </p>
+        <Link href="/">Retornar para a página inicial</Link>
+      </div>
+
+      {/* <Head>
+        <title>Monteiro Guincho de Moto - Página não encontrada</title>
+      </Head>
+      <div className={styles.notFound}>
+        <h1>Página não encontrada</h1>
+        <p>Opa, parece que essa página não está disponível.</p>
+        <Link href="/">Retornar para a página inicial</Link>
+      </div>
+     */}
+    </>
+  );
+};
 
 export default NotFound;
