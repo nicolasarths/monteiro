c9279292a37e807fcd0429ea4fddea9d76a51a13
nicolasarths <nicolasarths@hotmail.com>
2024-01-18 11:25:41 -0300

    redesign

--- src/common/pages/Homepage/PresentationSection/index.js
+++ src/common/pages/Homepage/PresentationSection/index.js
@@ -1,34 +1,64 @@
+import NextImage from "common/components/Next/Image";
 import styles from "./PresentationSection.module.sass";
 import { useState } from "react";
+import old from "./old.png";
+import blue from "./blue.png";
+import pedro from "./pedro.jpg";
+import orange from "./orange.jpg";
 
 const PresentationSection = () => {
   const [Clicked, setClicked] = useState(false);
-  const [text, setText] = useState("Assistir ao vídeo institucional");
+
+  const textBeforeClick = "Assistir ao vídeo";
+  const textAfterClick = "Esconder vídeo";
+
+  const [text, setText] = useState(textBeforeClick);
 
   const setAsClicked = () => {
     setClicked(!Clicked);
     setText((prev) =>
-      prev == "Fechar" ? "Assistir ao vídeo institucional" : "Fechar"
+      prev == textAfterClick ? textBeforeClick : textAfterClick
     );
   };
   return (
     <>
       <div className={styles.container}>
-        <p>
-          A Monteiro Guincho de Moto nasceu em 2016 com o intuito de se
-          estabelecer como referência no transporte especializado de
-          motocicletas na Grande Porto Alegre.
-        </p>
-        <p>
-          Desde então milhares de clientes satisfeitos já usufruíram da nossa
-          rampa automatizada com reboque basculante, que funciona 24 horas por
-          dia, 7 dias por semana!
-        </p>
-        <p>
-          Enguiçou? Ficou sem gasolina? Precisa ir fazer revisões, vistorias ou
-          transportar sua moto para o litoral? Socorro, resgate e leva e traz de
-          motos é com a Monteiro Guincho de Moto!
-        </p>
+        <div id="intro" className={styles.intro}>
+          <p className={styles.enguicou}>
+            <strong>Enguiçou?</strong> <br />
+            <strong>Ficou sem gasolina?</strong> <br />
+          </p>
+          <div className={styles.precisa}>
+            <p>Precisa:</p>
+            <ul className={styles.smaller}>
+              <li>fazer revisões</li>
+              <li>fazer vistorias</li>
+              <li>transportar a moto para o litoral?</li>
+            </ul>
+          </div>
+          <p className={styles.socorro}>
+            <strong>Socorro</strong>, <strong>resgate</strong> e{" "}
+            <strong>leva e traz de motos</strong> é com a Monteiro Guincho de
+            Moto!
+          </p>
+          <div className={styles.buttonContainer}>
+            <button>Fazer um orçamento</button>
+          </div>
+        </div>
+        <div id="historia" className={styles.historia}>
+          <h2>História</h2>
+          <p>
+            A <strong>Monteiro Guincho de Moto</strong> nasceu em 2016 com o
+            intuito de se estabelecer como <strong>referência</strong> no
+            transporte especializado de motocicletas na Grande Porto Alegre.
+          </p>
+          <NextImage className={styles.imageContainer} src={old} />
+          <p>
+            Desde então milhares de clientes satisfeitos já usufruíram da nossa
+            rampa automatizada com reboque basculante, que funciona 24 horas por
+            dia, 7 dias por semana!
+          </p>
+        </div>
         <h3>Regiões em que atuamos</h3>
         <ul>
           <li>Porto Alegre</li>
@@ -37,41 +67,49 @@ const PresentationSection = () => {
           <li>Viamão</li>
           <li>Canoas</li>
         </ul>
+        <p>
+          Atuamos com guincho de moto em Porto Alegre e região metropolitana, e
+          fazemos o transporte de motos para outras regiões.
+        </p>
+        <NextImage src={blue} className={styles.imageContainer} />
         <h3>Método de Trabalho</h3>
         <p>
           Damos urgência e cuidamos com toda a dedicação da sua moto,
           transportando-a com segurança e qualidade para onde você quiser
           levá-la. Pode contar com nossos serviços sempre que precisar.
         </p>
+        <NextImage src={pedro} className={styles.imageContainer} />
         <h4>Agendamentos</h4>
         <p>
           Aceitamos transportes agendados para moto para que você possa se
           programar na data e horário que melhor atendam às suas necessidades.
         </p>
+        <NextImage src={orange} className={styles.imageContainer} />
         <h4>Porte de Motocicletas</h4>
         <p>
           Trabalhamos no transporte especializado de motocicletas esportivas, de
           grande porte, para eventos, competições e viagens para o litoral,
-          interior e até mesmo transporte de motos para outros estados. Mas
-          também ajudamos você a chegar até a oficina quando precisar, para
-          realizar perícias ou consertos de urgência na sua moto.
+          interior e até mesmo transporte de motos para outros estados.
         </p>
-        <h4>Demonstração</h4>
-        <p>Assista ao vídeo de uma de nossas rampas funcionando!</p>
-
-        <button onClick={setAsClicked}>{text}</button>
-
-        {Clicked && (
-          <div className={styles.player}>
-            <iframe
-              src="https://www.youtube.com/embed/Zw9nMCUJNRY"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
-          </div>
-        )}
+        <p>
+          Ajudamos você a chegar até a oficina quando precisar, para realizar
+          perícias ou consertos de urgência na sua moto.
+        </p>
+        <h4>Vídeo Institucional</h4>
+        <p>
+          <button onClick={setAsClicked}>{text}</button>
+          {Clicked && (
+            <div className={styles.player}>
+              <iframe
+                src="https://www.youtube.com/embed/Zw9nMCUJNRY"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            </div>
+          )}
+        </p>
       </div>
     </>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,500&family=Raleway:wght@400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,900;1,500;1,700&display=swap"
             rel="stylesheet"
