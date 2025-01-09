611d3df506fd7ef781c56b1fe041a6cf84077536
Nícolas Arths <102503620+nicolasarths@users.noreply.github.com>
2025-01-09 20:34:41 -0300

    Update E-mail From Data

--- src/common/data/index.js
+++ src/common/data/index.js
@@ -2,7 +2,7 @@ export const hrefs = {
   SEND_WHATSAPP_MESSAGE:
     "https://wa.me/55051986561694?text=Ol%C3%A1%2C%20Monteiro%2C%20gostaria%20de%20um%20or%C3%A7amento.%20Segue%20o%20modelo%20da%20minha%20moto%2C%20a%20origem%20e%20o%20destino%20do%20transporte%3A",
   PHONE_NUMBER: "tel:051986561694",
-  SEND_MAIL: "mailto:contato@monteiromototransporte.com.br",
+  SEND_MAIL: "mailto:guinchodemoto@hotmail.com",
   DEV_WEBSITE: "https://www.nicolasarths.com.br/",
   CONTACT_VIA_WEBSITE: "/#contato",
   CONTACT_SUCCESS_PAGE: "/sucesso-ao-contatar",
